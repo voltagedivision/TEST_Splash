5be0e2b8b0310eb967d03ff0f8765766e048ec0c
Monse <msantillanrodriguez75@knights.ucf.edu>
2024-02-11 22:45:19 -0500

    splash screen testing

---
+++ app/src/main/java/com/example/test_splash/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.test_splash
+
+import android.graphics.Typeface
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.inputmethod.InputBinding
+import androidx.annotation.RequiresApi
+import android.widget.TextView
+import com.example.test_splash.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        val view = binding.root
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        setContentView(view)
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "Owl.ttf")
+        binding.tvAppName.typeface = typeFace
+    }
+}
